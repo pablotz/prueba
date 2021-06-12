c14fe28c45949c382d2ebe11d2a0071661c9ae73
PabloTz <pablotroll789@hotmail.com>
2021-06-11 19:49:27 -0500

    Cambios desde develop

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Codigo modificado en la carpeta de prueba2</h1>
-    <h1>Creado desde la carpeta de prueba1</h1>
+    <h1>Codigo modificado desde rama develop</h1>
 </body>
 </html>
